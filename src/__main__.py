0a1de57b0fd76a4bde39b331f2c6e34aa7f282e3
Judith Wegmann <judith.wegmann@kit.edu>
2024-09-19 20:51:48 +0200

    simple Projektstruktur

---
+++ src/__main__.py
@@ -0,0 +1,5 @@
+import sys
+
+if __name__=="__main__":
+    from corynesegmentation import project_entry
+    sys.exit(project_entry())
---
+++ src/corynesegmentation.py
@@ -0,0 +1,9 @@
+def project_entry():
+    print("Hello world")
+
+import mkdocs
+
+import numpy
+
+if mkdocs:
+    print('wuhu')
